d0e7eb49e0a4113ac5c2c2d805e1feebf8b4793e
M4teus2020 <mfelini7@gmail.com>
2023-12-29 00:52:57 -0300

    feat: HomeBanner and categories

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,40 +1,14 @@
 <template>
     <div class="mt-[-120px]">
+        <HomeBanner />
         <div
-            class="flex justify-center align-center overflow-hidden h-[500px] select-none md:h-max"
-        >
-            <div class="grid grid-cols-9 gap-2 pt-2.5 flex-none">
-                <img
-                    class="h-[9rem] lg:h-[13rem]"
-                    v-for="i in 36"
-                    :src="require(`@/assets/imgs/movies/movie (${i}).png`)"
-                    :key="i"
-                />
-            </div>
-            <img
-                class="absolute w-full h-[500px] md:h-[600px] lg:h-[800px] blend"
-                src="@/assets/imgs/background.png"
-            />
-            <div
-                class="backdrop absolute w-full h-[400px] md:h-[580px] self-start"
-            ></div>
-            <div
-                class="backdrop absolute w-full h-[400px] md:h-[580px] self-end rotate-180 mb-[-10px]"
-            ></div>
-            <div
-                class="absolute p-28 md:p-20 max-w-[450px] lg:max-w-[580px] shrink"
-            >
-                <img src="@/assets/imgs/design.svg" />
-            </div>
-        </div>
-        <div
-            class="flex flex-column justify-center align-center top-[-4rem] relative gap-20 max-w-6xl m-auto px-6 md:px-16"
+            class="flex flex-column justify-center align-center top-[-6rem] lg:top-[-4rem] relative gap-10 max-w-6xl m-auto px-4 md:px-16"
         >
             <div class="flex flex-column gap-3.5 text-center">
                 <h1 class="text-3xl lg:text-6xl font-bold leading-normal">
                     The Best Streaming Experience
                 </h1>
-                <p class="text-lg text-center">
+                <p class="lg:text-lg text-grey-600">
                     StreamVibe is the best streaming experience for watching
                     your favorite movies and shows on demand, anytime, anywhere.
                     <span class="hidden md:inline">
@@ -53,16 +27,18 @@
             </button>
         </div>
     </div>
-    <div>
+    <div class="mx-4">
         <div>
-            <p>Explore our wide variety of categories</p>
-            <p>
+            <p class="text-2xl font-bold">
+                Explore our wide variety of categories
+            </p>
+            <p class="text-sm text-grey-600 mt-3">
                 Whether you're looking for a comedy to make you laugh, a drama
                 to make you think, or a documentary to learn something new
             </p>
         </div>
-        <div>
-            <div class="flex gap-10">
+        <div class="mt-10">
+            <div class="flex gap-10 overflow-scroll">
                 <div
                     v-for="category in [
                         'action',
@@ -72,15 +48,22 @@
                         'horror',
                     ]"
                     :key="category"
+                    class="border-2 border-black-150 rounded-[12px] p-5 bg-black-100 box-border"
                 >
-                    <div class="grid grid-cols-2">
+                    <div class="grid grid-cols-2 gap-1 w-[140px]">
                         <img
                             v-for="i in 4"
                             :src="
                                 require(`@/assets/imgs/movies/${category} (${i}).png`)
                             "
                             :key="i"
+                            class="rouded-lg"
                         />
+                    </div>
+                    <div class="dropdown" />
+                    <div class="flex justify-between">
+                        <p class="capitalize">{{ category }}</p>
+                        <ArrowRightIcon class="w-5" />
                     </div>
                 </div>
             </div>
@@ -88,12 +71,15 @@
     </div>
 </template>
 <script lang="ts">
-import { PlayIcon } from "@heroicons/vue/24/solid";
+import { PlayIcon, ArrowRightIcon } from "@heroicons/vue/24/solid";
 import { defineComponent } from "vue";
+import HomeBanner from "./components/HomeBanner.vue";
 
 export default defineComponent({
     components: {
+        HomeBanner,
         PlayIcon,
+        ArrowRightIcon,
     },
     data() {
         return {};
@@ -102,13 +88,16 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.backdrop {
-    background: linear-gradient(180deg, #141414 0%, rgba(20, 20, 20, 0) 100%);
-}
-
-.blend {
-    background: url(../../assets/imgs/background.png);
-    background-blend-mode: overlay;
-    mix-blend-mode: overlay;
+.dropdown:after {
+    width: 193px;
+    height: 210px;
+    position: relative;
+    left: -27.8px;
+    top: -70px;
+    background: linear-gradient(
+        180deg,
+        rgba(26, 26, 26, 0) 0%,
+        #1a1a1a 101.79%
+    );
 }
 </style>
---
+++ src/views/home/components/HomeBanner.vue
@@ -0,0 +1,50 @@
+<template>
+    <div
+        class="flex justify-center align-center overflow-hidden h-[500px] select-none md:h-max"
+    >
+        <div class="grid grid-cols-9 gap-2 pt-2.5 flex-none mt-20 md:mt-0">
+            <img
+                class="h-[9rem] lg:h-[13rem]"
+                v-for="i in 36"
+                :src="require(`@/assets/imgs/movies/movie (${i}).png`)"
+                :key="i"
+            />
+        </div>
+        <img
+            class="absolute w-full h-[500px] md:h-[600px] lg:h-[800px] blend"
+            src="@/assets/imgs/background.png"
+        />
+        <div
+            class="backdrop absolute w-full h-[400px] md:h-[580px] self-start"
+        ></div>
+        <div
+            class="backdrop absolute w-full h-[400px] md:h-[580px] self-end rotate-180 mb-[-10px]"
+        ></div>
+        <div
+            class="absolute p-28 md:p-20 max-w-[450px] lg:max-w-[580px] shrink"
+        >
+            <img src="@/assets/imgs/design.svg" />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    data() {
+        return {};
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.backdrop {
+    background: linear-gradient(180deg, #141414 0%, rgba(20, 20, 20, 0) 100%);
+}
+
+.blend {
+    background: url(@/assets/imgs/background.png);
+    background-blend-mode: overlay;
+    mix-blend-mode: overlay;
+}
+</style>
